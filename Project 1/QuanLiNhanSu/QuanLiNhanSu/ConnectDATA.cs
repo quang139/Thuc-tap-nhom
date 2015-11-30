d8cc470a08cd4b8e885a28c6a8be4e1bb45d4881
hoanghieuthao <tuananh16592@gmail.com>
2015-11-30 09:29:20 +0700

    sủa 1

---
+++ Project 1/QuanLiNhanSu/QuanLiNhanSu/ConnectDATA.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Management;
+namespace QuanLiNhanSu
+{
+    class ConnectDATA
+    {
+        public static string getconnect()
+        {
+            string str = ConfigurationManager.ConnectionStrings["ConnectQLNS"].ToString();
+            return str;
+        }
+    }
+}
