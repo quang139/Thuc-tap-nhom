617b32759716bec7b369310a9df414384e80bad1
hoanghieuthao <tuananh16592@gmail.com>
2015-12-01 09:49:30 +0700

    laun ngu

---
+++ Project_1/QuanLiNhanSu/QuanLiNhanSu/QuanLiNhanSu/ConnectDATA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLiNhanSu
+{
+    class ConnectDATA
+    {
+        public static string getconnect()
+        {
+            return (@"Data Source=LUANJR11-PC\LUANJR_SQL;Initial Catalog=QuanLiNhanSu;Integrated Security=True");
+        }
+    }
+}
---
+++ Project_1/QuanLiNhanSu/QuanLiNhanSu/QuanLiNhanSu/DangNhap.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace QuanLiNhanSu
+{
+    public partial class frmDangNhap : Form
+    {
+        public frmDangNhap()
+        {
+            InitializeComponent();
+        }
+        private SqlConnection con;
+        DataTable dt;
+        private SqlDataAdapter da = new SqlDataAdapter();
+        private void connect()
+        {
+            try
+            {
+                con = new SqlConnection(ConnectDATA.getconnect());
+                con.Open();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Không thể kết nối cơ sở dữ liệu");
+            }
+        }
+
+        private void DangNhap_Load(object sender, EventArgs e)
+        {
+            connect();
+        }
+
+        private void bntOK_Click(object sender, EventArgs e)
+        {
+            SqlCommand command = new SqlCommand();
+            command.Connection = con;
+            command.CommandType = CommandType.Text;
+            command.CommandText = "Select * From tblDangNhap Where (Taikhoan=@user) And (matkhau=@password)";
+            command.Parameters.Add("@user", SqlDbType.NVarChar, 50).Value = txtTaiKhoan.Text;
+            command.Parameters.Add("@password", SqlDbType.NVarChar, 50).Value = txtMatKhau.Text;
+            da.SelectCommand = command;
+            da.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                MessageBox.Show("Dang nhap thanh cong");
+                Hide();
+                frmMain main = new frmMain();
+                main.Show();
+            }
+            else
+            {
+                MessageBox.Show("Đăng nhập thất bại");
+            }
+        }
+       
+    }
+}
